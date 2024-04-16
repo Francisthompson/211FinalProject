e833a400912ed5463e15d77106d3703616e1f750
Francis <francis5@shaw.ca>
2024-04-16 12:26:28 -0600

    Add project files.

---
+++ MauiApp29/Components/Data/Reservation.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+namespace EndToEnd.Data;
+public class Reservation
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Customer { get; set; }
+
+    public string Time { get; set; }
+
+    public int Table { get; set; }
+}
---
+++ MauiApp29/Components/Data/ReservationService.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace EndToEnd.Data;
+public class ReservationService
+{
+    string _dbPath;
+    public string StatusMessage { get; set; }
+    private SQLiteAsyncConnection conn;
+    public ReservationService(string dbPath)
+    {
+        _dbPath = dbPath;
+    }
+    private async Task InitAsync()
+    {
+        // Don't Create database if it exists
+        if (conn != null)
+            return;
+        // Create database and Reservation Table
+        conn = new SQLiteAsyncConnection(_dbPath);
+        await conn.CreateTableAsync<Reservation>();
+    }
+    public async Task<List<Reservation>> GetReservationsAsync()
+    {
+        await InitAsync();
+        return await conn.Table<Reservation>().ToListAsync();
+    }
+    public async Task<Reservation> CreateReservationAsync(
+        Reservation reservation)
+    {
+        // Insert
+        await conn.InsertAsync(reservation);
+        return reservation;
+    }
+    public async Task<Reservation> UpdateReservationAsync(
+        Reservation reservation)
+    {
+        // Update
+        await conn.UpdateAsync(reservation);
+        // Return the updated object
+        return reservation;
+    }
+    public async Task<Reservation> DeleteReservationAsync(
+        Reservation reservation)
+    {
+        // Delete
+        await conn.DeleteAsync(reservation);
+        return reservation;
+    }
+}
